aa73f9ce9a2cac789aa7819d0a479148558aa269
cmreakaya <cemreakkaya58@gmail.com>
2024-02-14 21:26:25 +0300

    first commit

---
+++ Home.js
@@ -0,0 +1,9 @@
+import {SafeAreaView,Text,StyleSheet} from 'react-native';
+
+export default function Home ({navigation}) {
+  return (
+    <SafeAreaView>
+     <Text>Home sayfasına Hoşgeldiniz!</Text>
+    </SafeAreaView>
+  );
+}
